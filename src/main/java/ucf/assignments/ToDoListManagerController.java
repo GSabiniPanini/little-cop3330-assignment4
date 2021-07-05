4143e30e6199f720cd9c91bc7a93e04da1b9e0db
Glenroy Little <glittle02@knights.ucf.edu>
2021-07-05 05:05:30 -0500

    Messing with files

--- src/main/java/ucf/assignments/ToDoListManagerController.java
+++ src/main/java/ucf/assignments/ToDoListManagerController.java
@@ -1,12 +1,13 @@
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.BorderPane;
@@ -15,9 +16,7 @@ import javafx.scene.layout.HBox;
 
 public class ToDoListManagerController {
 
-    //gui initializations
     @FXML private BorderPane BorderPane;
-    @FXML private MenuBar MenuBar;
     @FXML private Menu FileMenu;
     @FXML private MenuItem FileSaveList;
     @FXML private MenuItem FileSaveAll;
@@ -25,10 +24,11 @@ public class ToDoListManagerController {
     @FXML private MenuItem FileLoadCollection;
     @FXML private Menu FilterMenu;
     @FXML private RadioMenuItem ShowAllFilter;
+    @FXML private ToggleGroup FilterToggleGroup;
     @FXML private RadioMenuItem ShowDoneFilter;
     @FXML private RadioMenuItem ShowNotDoneFilter;
     @FXML private GridPane LeftGridPane;
-    @FXML private ListView<?> ToDoLists;
+    @FXML private ListView<?> ToDoListView;
     @FXML private HBox ListsHBox;
     @FXML private Button AddListButton;
     @FXML private Button RemoveListButton;
@@ -45,7 +45,68 @@ public class ToDoListManagerController {
     @FXML private Button RemoveItemButton;
 
 
-    @FXML void removeitem(ActionEvent event) {
+    @FXML
+    void AddListButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditItemButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RemoveListButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RenameListButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowAllFilterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowDoneFilterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowNotDoneFilterClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addItemButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuLoadCollectionClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuLoadListClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuSaveAllClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuSaveListClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeItemButtonClicked(ActionEvent event) {
 
     }
 
